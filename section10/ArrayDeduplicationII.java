8de2f7b64698185945aac1a66322bdaa596d1171
Chu Wang <cwang9@wpi.edu>
2019-11-04 13:53:28 -0800

    1104

--- section10/ArrayDeduplicationII.java
+++ section10/ArrayDeduplicationII.java
@@ -1,7 +1,30 @@
 package section10;
 
+import java.util.Arrays;
+
 /**
  * sorted array, duplicate element only retain two
  */
+
 public class ArrayDeduplicationII {
+    public int[] dedup(int[] array) {
+        // Write your solution here
+        if(array.length <= 2){
+            return array;
+        }
+        int slow = 2;
+        for(int fast = 2; fast < array.length; fast++){
+            if(array[slow - 2] != array[fast]){
+                array[slow++] = array[fast];
+            }
+        }
+        return Arrays.copyOf(array, slow+1);
+    }
+    public static void main(String[] args){
+        ArrayDeduplicationII solution = new ArrayDeduplicationII();
+        int[] array = new int[]{1, 1, 1, 2, 3};
+        int[] deduparray = solution.dedup(array);
+        System.out.println(Arrays.toString(deduparray));
+
+    }
 }
--- section10/ArrayDeduplicationIII.java
+++ section10/ArrayDeduplicationIII.java
@@ -1,7 +1,40 @@
 package section10;
 
+import java.util.Arrays;
+
 /**
  * sorted array, duplicate element not retain any
  */
 public class ArrayDeduplicationIII {
+    public int[] dedup(int[] array){
+        //Assumption: array is not null
+        if(array == null || array.length <= 1){
+            return array;
+        }
+
+        int end = 0;
+        boolean flag = false;
+        for(int i = 1; i < array.length; i++){
+            if(array[i] == array[end]){
+                //if there is duplicate, set flag and do nothign
+                flag = true;
+            } else if(flag == true){
+                //if array[i] != array[end] and flag is set
+                // array[end] should not be be included in the valid subarray
+                //and we can just replace it with array[i]
+                //since next we are going to check if there is
+                //any duplicate of array[i]
+                array[end] = array[i];
+                flag = false;
+            } else {
+                //if array[i] != array[end]
+                //and flag is not set
+                //it means there is no duplicate of array[end] and it should be
+                //included int he valid subarray
+                array[++end] = array[i];
+            }
+        }
+        return Arrays.copyOf(array, flag ? end : end + 1);
+
+    }
 }
