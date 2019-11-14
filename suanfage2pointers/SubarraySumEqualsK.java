9eef8efd4334fd3cf5edec3bc7f718ba613f4daa
Chu Wang <cwang9@wpi.edu>
2019-11-14 14:22:26 -0800

    1113

---
+++ suanfage2pointers/SubarraySumEqualsK.java
@@ -0,0 +1,33 @@
+package suanfage2pointers;
+
+public class SubarraySumEqualsK {
+    /**
+     * optimize space complexity to O(1)
+     * 
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        // int[] sum = new int[nums.length + 1];
+        // sum[0] = 0;
+
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                if(sum == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        SubarraySumEqualsK solution = new SubarraySumEqualsK();
+        int[] array = new int[]{2, 4, 5,6, 1};
+        int result = solution.subarraySum(array, 5);
+        System.out.println(result);
+    }
+}
---
+++ suanfageTree/CriticalEdges.java
@@ -0,0 +1,19 @@
+package suanfageTree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CriticalEdges {
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections){
+        return null;
+    }
+
+    public static void main(String[] args){
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        for(Integer i : array){
+            System.out.println(i);
+        }
+    }
+}
